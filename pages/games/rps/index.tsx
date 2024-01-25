7fe437234a5f1a2eff6478611d95b752673c9728
Richard Izip <richardizip@gmail.com>
2024-01-25 14:49:08 +0100

    feat: add testing actions

--- pages/games/rps/index.tsx
+++ pages/games/rps/index.tsx
@@ -5,11 +5,15 @@ import { navHeight } from 'constants/global'
 import NextLink from 'next/link'
 import { brandButtonStyle } from 'theme/simple'
 import ValidateGate from 'components/validate-gate'
+import { getApi } from 'utils/lucid/lucid';
+import { useSession } from 'next-auth/react';
+
+const commonButtonProps = {...brandButtonStyle, mt:'10px', w:'350px', mb:'10px', h:'50px' }
 
 const OptionButton = ({ message, href }: { message: string, href: string }) => {
   return (
     <NextLink href={href} passHref>
-      <Button as="a" {...brandButtonStyle} mt='10px' w='350px' mb='10px' h='50px'>
+      <Button as="a" {...commonButtonProps}>
         {message}
       </Button>
     </NextLink>
@@ -17,14 +21,43 @@ const OptionButton = ({ message, href }: { message: string, href: string }) => {
 }
 
 const Home: NextPage = () => {
+  const {data} = useSession()
   const router = useRouter()
   const query = router.query;
+
+  const wallet = data?.user.wallet
+
   return (
     <ValidateGate>
       <Flex direction='column' justify='center' h={`calc(100vh - ${navHeight})`} align='center'>
         {query?.completed === "true" && <Heading variant='brand' mb='20px'>Game is completed</Heading>}
         <OptionButton message={"Create a new game"} href="/games/rps/new-game" />
         <OptionButton message="Join an active game involving you" href='/games/rps/active-games' />
+
+        {wallet != null && (
+          <>
+            <div style={{marginTop: 60}} />
+            <Button {...commonButtonProps} onClick={async () => {
+                const api = await getApi(wallet)
+                await api.getCollateral()
+              }}>
+              Testing: Create collateral
+            </Button>
+            <Button {...commonButtonProps}  onClick={async () => {
+              const api = await getApi(wallet)
+              // This provides encoded address of selected account.
+              // There is probably better way to access the address.
+              // This is solely for convenience.
+              const address = (await api.getChangeAddress())
+              const signedMessage = await api.signData(address, Buffer.from('payload').toString(
+                'hex',
+              ))
+              console.log('signedMessage output: ', signedMessage)
+            }}>
+              Testing: Sign Message
+            </Button>
+          </>
+        )}
       </Flex>
     </ValidateGate>
   )
